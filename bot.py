971f047e02a4aeffb11258da86537fa5e6937169
Andrew059 <71473456+Andrew059@users.noreply.github.com>
2022-03-18 16:05:11 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import discord
-from db import message_database, koins_database, community_koins_database, total_messages_sent
+import redis
 from blockchain import Blockchain
 import json
 import os
 client = discord.Client()
-TOKEN="OTM0MTIwMzY5NDY2NTMxODUw.YerdYw.vl7K9qhD9GbOMJFaWKtWSfOjeKo"
+TOKEN="fdsinfsofnsdfon"
+r = redis.Redis(host='localhost', port=6379, db=0)
 
 try:
     os.remove("blockchain.json")
@@ -13,11 +14,11 @@ except:
 file = open("blockchain.json", "x")
 file.close()
 
-data2 = {
+init = {
     "blockchain": []
 }
   
-json_object = json.dumps(data2, indent = 4)
+json_object = json.dumps(init, indent = 4)
 
 with open("blockchain.json", "w") as outfile:
     outfile.write(json_object)
@@ -31,6 +32,8 @@ blockchain.update_blocks()
 # Prizes can be discord nitro, kurius merch, etc
 # Transaction between users, monthly allowance, and prize redemption
 # Find way to count Koins based on blockchain
+# Have a store directly accessible via Discord (Transactions occur in the blockchain)
+
 
 @client.event
 async def on_ready():
@@ -43,193 +46,107 @@ async def on_message(message):
     if message.author == client.user:
         return
     else:
-        total_messages_sent.append("")
+        if (r.get("total_messages_sent") == None):
+            r.set("total_messages_sent", 1)
+        else:
+            number_of_messages = r.get('total_messages_sent')
+            number_of_messages2 = int(number_of_messages.decode('utf-8')) + 1
+            r.set("total_messages_sent", number_of_messages2)
+            #Equivalent to total_messages_sent
+    user_id = message.author.id
     split_message = message.content.split(" ")
 
-    if len(message_database) == 0:
-        new_user = {message.author.id: 1}
-        message_database.append(new_user)
+    if (r.hget("messages_sent", user_id) == None):
+        print(True)
+        r.hset("messages_sent", user_id, 1)
     else:
-        num2 = 0
-        for i in range(len(message_database)):
-            if str(message_database[i].keys()) == "dict_keys([" + str(message.author.id) + "])":
-                message_database[i][message.author.id] += 1
-                num2 = 1
-        if num2 != 1:
-            new_user = {message.author.id: 1}
-            message_database.append(new_user)
+        result = r.hget('messages_sent', user_id)
+        result2 = int(result.decode('utf-8')) + 1
+        r.hset("messages_sent", user_id, result2)
     
-    for i in range(len(message_database)):
-        if str(message_database[i].keys()) == "dict_keys([" + str(message.author.id) + "])":
-            if message_database[i][message.author.id] % 1 == 0: # For every 30 messages sent by the user, the user will receive one Koins
-                if len(koins_database) == 0:
-                    new_user = {message.author.id: 1}
-                    koins_database.append(new_user)
-                    block_transaction = {"sender": str(client.user.name), "receiver": str(await client.fetch_user(message.author.id)), "amount": 1}
-                    blockchain.add_block(block_transaction)
-                    blockchain.update_blocks()
-                else:
-                    num3 = 0
-                    for k in range(len(koins_database)):
-                        if str(koins_database[k].keys()) == "dict_keys([" + str(message.author.id) + "])":
-                            # ADDS KOINS
-                            block_transaction = {"sender": str(client.user.name), "receiver": str(await client.fetch_user(message.author.id)), "amount": 1}
-                            blockchain.add_block(block_transaction)
-                            num3 = 1
-                            koins_database[k][message.author.id] += 1
-                            blockchain.update_blocks()
-                    if num3 != 1:
-                        block_transaction = {"sender": str(client.user.name), "receiver": str(await client.fetch_user(message.author.id)), "amount": 1}
-                        blockchain.add_block(block_transaction)
-                        new_user = {message.author.id: 1}
-                        koins_database.append(new_user)
-                        blockchain.update_blocks()
+    messages_sent_by_user_string = r.hget('messages_sent', user_id)
+    messages_sent_by_user_int = int(messages_sent_by_user_string.decode('utf-8'))
 
-    if len(total_messages_sent) % 1 == 0: # For every 500 messages sent by users, each user will receive 30 Community Koins
-        for i in range(len(message_database)):
-            key = str(message_database[i].keys())
-            key2 = key.replace("dict_keys([", "")
-            key3 = key2.replace("])", "")
-            key4 = key3.replace(",", "")
-            key4_num = int(key4)
-            num7 = 0
-            for c in range(len(community_koins_database)):
-                if str(message_database[c].keys()) == "dict_keys([" + key4 + "])":
-                    community_koins_database[c][key4_num] += 30
-                    recipient_name = await client.fetch_user(key4_num)
-                    block_transaction = {"sender": str(client.user.name), "receiver": str(recipient_name), "amount": 30}
-                    blockchain.add_block(block_transaction)
-                    blockchain.update_blocks()
-                    num7 = 1
-            if num7 != 1:
-                new_user = {key4_num: 30}
-                community_koins_database.append(new_user)
-                recipient_name = await client.fetch_user(key4_num)
-                block_transaction = {"sender": str(client.user.name), "receiver": str(recipient_name), "amount": 30}
-                blockchain.add_block(block_transaction)
-                blockchain.update_blocks()
+    if (messages_sent_by_user_int % 30 == 0):
+        if (r.hget("number_of_koins", user_id) == None):
+            r.hset('number_of_koins', user_id, 1)
+            block_transaction = {"sender": str(client.user.name), "receiver": str(await client.fetch_user(message.author.id)), "amount": 1}
+            blockchain.add_block(block_transaction)
+            blockchain.update_blocks()
+        else:
+            result = r.hget('number_of_koins', user_id)
+            result2 = int(result.decode('utf-8')) + 1
+            r.hset("number_of_koins", user_id, result2)
+            block_transaction = {"sender": str(client.user.name), "receiver": str(await client.fetch_user(message.author.id)), "amount": 1}
+            blockchain.add_block(block_transaction)
+            blockchain.update_blocks()
 
-    if split_message[0] == '$Koins':
-        num5 = 0
-        for k in range(len(koins_database)):
-                if str(koins_database[k].keys()) == "dict_keys([" + str(message.author.id) + "])":
-                    account_value =  koins_database[k][message.author.id]
-                    num5 = 1
-        if num5 != 1:
-            account_value = 0
-        await message.channel.send('```You have ' + str(account_value)  + " Koins!```")
+    total_messages_sent_string = r.get('total_messages_sent')
+    total_messages_sent_int = int(total_messages_sent_string.decode('utf-8'))
+    koins_array = r.hkeys("number_of_koins")
 
-    if split_message[0] == "$TotalServerMessages":
-        await message.channel.send('```' + str(len(total_messages_sent)) + " messages have been sent!```")
+    if (total_messages_sent_int % 200 == 0):
+        for c in range(len(koins_array)):
+            key = str(koins_array[c].decode('utf-8'))
+            if (r.hget("number_of_community_koins", key) == None):
+                r.hset("number_of_community_koins", key, 30)
+            else:
+                r.hincrby("number_of_community_koins", key, 30)
+            user_id = int(key)
+            recipient_name = await client.fetch_user(user_id)
+            block_transaction = {"sender": str(client.user.name), "receiver": str(recipient_name), "amount": 30}
+            blockchain.add_block(block_transaction)
+            blockchain.update_blocks()
 
-    if split_message[0] == '$Messages':
-        num6 = 0
-        for i in range(len(message_database)):
-            if str(message_database[i].keys()) == "dict_keys([" + str(message.author.id) + "])":
-                messages_sent = message_database[i][message.author.id]
-                num6 = 1
-        if num6 != 1:
-            messages_sent = 1
-        await message.channel.send('```You have sent ' + str(messages_sent) + " messages!```")
+    if split_message[0] == "$totalServerMessages":
+        total_number_of_messages = r.get('total_messages_sent')
+        total_number_of_messages_string = str(total_number_of_messages.decode('utf-8'))
+        await message.channel.send('```' + total_number_of_messages_string + " messages have been sent!```")
+
+    if split_message[0] == '$messages':
+        messages_sent_by_specific_user = r.hget('message_sent', user_id)
+        messages_sent_by_specific_user_string = str(messages_sent_by_user_string.decode('utf-8'))
+        await message.channel.send('```You have sent ' + messages_sent_by_specific_user_string + " messages!```")
     
-    if split_message[0] == '$ShowBlockchain':
+    if split_message[0] == '$showBlockchain':
         await message.channel.send(file=discord.File('blockchain.json'))
     
-    if split_message[0] == '$GiftKoins':
-        num8 = 0
+    if split_message[0] == '$giftCommunityKoins':
         amount_of_money = split_message[1]
-        amount_of_money_num = int(amount_of_money)
+        amount_of_money_int = int(amount_of_money)
         tagged_recipient = split_message[2]
         recipient_id =  tagged_recipient.replace("<@!","")
         final_recipient_id = recipient_id.replace(">","")
         final_recipient_id_num = int(final_recipient_id)
-        recipient_name = await client.fetch_user(final_recipient_id_num)
         sender_id = message.author.id
-        if final_recipient_id_num == sender_id:
-            await message.channel.send('```You cannot gift yourself your own Koins!```')
-            num8 = 1
-        for k in range(len(koins_database)):
-                        if str(koins_database[k].keys()) == "dict_keys([" + str(message.author.id) + "])":
-                            account_value =  koins_database[k][message.author.id]
-                            if account_value < amount_of_money_num:
-                                await message.channel.send("```You don't have enough Koins!```")
-                                num8 = 1
-                            else:
-                                koins_database[k][message.author.id] -= amount_of_money_num
-        if len(koins_database) == 0 and num8 == 0:
-            new_user = {final_recipient_id_num: amount_of_money_num}
-            koins_database.append(new_user)
+        amount_money_owned_by_sender = r.hget("number_of_community_koins", user_id)
+        amount_money_owned_by_sender_int = int(amount_money_owned_by_sender.decode('utf-8'))
+        recipient_name = await client.fetch_user(final_recipient_id_num)
+
+        if (r.hget("number_of_community_koins", user_id) == None):
+            await message.channel.send("You do not have any Community Koins in your name")
+
+        elif final_recipient_id_num == sender_id:
+            await message.channel.send('```You cannot gift yourself your own Community Koins!```')
+        
+        elif (amount_money_owned_by_sender_int < amount_of_money_int):
+            await message.channel.send("You do not have enough Community Koins")
+        
+        elif (r.hget("number_of_koins",final_recipient_id_num) == None):
+            r.hincrby("number_of_community_koins", sender_id, -amount_of_money_int)
+            r.hset("number_of_koins", final_recipient_id_num, amount_of_money_int)
+        else:
+            r.hincrby("number_of_community_koins", sender_id, -amount_of_money_int)
+            r.hincrby("number_of_koins", final_recipient_id_num, amount_of_money_int)
             block_transaction = {"sender": str(await client.fetch_user(message.author.id)), "receiver": str(recipient_name), "amount": amount_of_money}
             blockchain.add_block(block_transaction)
-            blockchain.update_blocks()     
-        elif num8 == 0:
-            for k in range(len(koins_database)):
-                if str(koins_database[k].keys()) == "dict_keys([" + str(final_recipient_id) + "])":
-                    block_transaction = {"sender": str(await client.fetch_user(message.author.id)), "receiver": str(recipient_name), "amount": amount_of_money}
-                    blockchain.add_block(block_transaction)
-                    koins_database[k][final_recipient_id_num] += amount_of_money_num
-                    blockchain.update_blocks()
-                    num = 1
-            if num != 1:
-                block_transaction = {"sender": str(await client.fetch_user(message.author.id)), "receiver": str(recipient_name), "amount": amount_of_money}
-                blockchain.add_block(block_transaction)
-                new_user = {final_recipient_id_num: amount_of_money_num}
-                koins_database.append(new_user)
-                blockchain.update_blocks()
-                num = 0
-        if num8 == 0:
-            await message.channel.send('```You have gifted ' + str(recipient_name) + ' ' + amount_of_money + ' Koins!```')
-    
-    if split_message[0] == '$GiftCommunityKoins':
-        num9 = 0
-        amount_of_money = split_message[1]
-        amount_of_money_num = int(amount_of_money)
-        tagged_recipient = split_message[2]
-        recipient_id =  tagged_recipient.replace("<@!","")
-        final_recipient_id = recipient_id.replace(">","")
-        final_recipient_id_num = int(final_recipient_id)
-        recipient_name = await client.fetch_user(final_recipient_id_num)
-        sender_id = message.author.id
-        if final_recipient_id_num == sender_id:
-            await message.channel.send('```You cannot gift yourself your own Koins!```')
-            num9 = 1
-        for k in range(len(community_koins_database)):
-                if str(community_koins_database[k].keys()) == "dict_keys([" + str(message.author.id) + "])":
-                    account_value =  community_koins_database[k][message.author.id]
-                    if account_value < amount_of_money_num:
-                        await message.channel.send("```You don't have enough Koins!```")
-                        num9 = 1
-                    else:
-                        community_koins_database[k][message.author.id] -= amount_of_money_num
-        if len(community_koins_database) == 0 and num9 == 0:
-            new_user = {final_recipient_id_num: amount_of_money_num}
-            koins_database.append(new_user)
-            block_transaction = {"sender": str(await client.fetch_user(message.author.id)), "receiver": str(recipient_name), "amount": amount_of_money}
-            blockchain.add_block(block_transaction)
-            blockchain.update_blocks()              
-        elif num9 == 0:
-            for k in range(len(community_koins_database)):
-                if str(community_koins_database[k].keys()) == "dict_keys([" + str(final_recipient_id) + "])":
-                    block_transaction = {"sender": str(await client.fetch_user(message.author.id)), "receiver": str(recipient_name), "amount": amount_of_money}
-                    blockchain.add_block(block_transaction)
-                    koins_database[k][final_recipient_id_num] += amount_of_money_num
-                    blockchain.update_blocks()
-                    num = 1
-            if num != 1:
-                block_transaction = {"sender": str(await client.fetch_user(message.author.id)), "receiver": str(recipient_name), "amount": amount_of_money}
-                blockchain.add_block(block_transaction)
-                new_user = {final_recipient_id_num: amount_of_money_num}
-                koins_database.append(new_user)
-                blockchain.update_blocks()
-                num = 0
-        if num9 == 0:
-            await message.channel.send('```You have gifted ' + str(recipient_name) + ' ' + amount_of_money + ' Koins!```')
+            blockchain.update_blocks()
 
-
-    print(len(total_messages_sent))
-    print(message_database)
-    print(koins_database)
-    print(community_koins_database)
+    print(r.get("total_messages_sent"))
+    print(r.hgetall("messages_sent"))
+    print(r.hgetall("number_of_koins"))
+    print(r.hgetall("number_of_community_koins"))
     # blockchain.print_blocks()
 
 client.run(TOKEN)
+# C:/Users/miste/AppData/Local/Programs/Python/Python39/python.exe c:/Users/miste/Downloads/KuriOS-main/KoinsBot/bot.py
